d1a7210df673822b2be24e76c3c10bbc2cf15dc8
gdezan <33992304+gdezan@users.noreply.github.com>
2018-04-13 11:45:22 -0300

    added ajax functionality and made some minor changes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	var id = "https://wind-bow.glitch.me/twitch-api/channels/freecodecamp"
+	var displayName = ["freecodecamp", "streamerhouse", "shroud"]
+	for (var i=0; i<displayName.length; i++){
+		var api = "https://wind-bow.glitch.me/twitch-api/channels/"+displayName[i];
+		$.ajax({
+			type: 'GET',
+			url: api,
+			datatype: "json",
+			success: function(data){
+				$("#streamers").append("<li class=\"stream\"><img class=\"logo\" src=\""+data.logo+"\"><span class=\"stream_name\">"+data.display_name+"</span></li>");
+		}
+	});
+	}
 });
