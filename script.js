663f87da32f93bdf652a65045a9f15dacfebf522
gdezan <33992304+gdezan@users.noreply.github.com>
2018-04-14 03:02:29 -0300

    fixed json hierarchy

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 $(document).ready(function() {
-	var displayName = ["freecodecamp", "streamerhouse", "shroud"]
-	displayName.forEach(function(name){
-		var channelApi = "https://wind-bow.glitch.me/twitch-api/channels/"+name;
-		var streamApi = "https://wind-bow.glitch.me/twitch-api/streams/"+name;
-		var stream = '';
-		var html = '';
-		$.getJSON(streamApi, function(data){
-			data3 = data;
-			if (data.stream ===  null) {
-				stream = "Offline";
-				status = "offline"
-			} else {
-				stream = "Playing "+data3.stream.game+" - "+data3.stream.viewers+" viewers";
-				status = "online"
-			}
+	var displayName = ["freecodecamp", "streamerhouse", "shroud", "swimstrim"];
+	function create(){
+		displayName.forEach(function(name){
+			var channelApi = "https://wind-bow.glitch.me/twitch-api/channels/"+name;
+			var streamApi = "https://wind-bow.glitch.me/twitch-api/streams/"+name;
+			var stream = '';
+			var html = '';
+			$.getJSON(streamApi, function(data){
+				var status = '';
+				statusData = data;
+				if (statusData.stream ===  null) {
+					stream = "Offline";
+					status = "offline";
+				} else {
+					stream = "Playing "+statusData.stream.game+" - "+statusData.stream.viewers+" viewers";
+					status = "online";
+				}
+
+				$.getJSON(channelApi,function(data2){
+					channel = data2;
+					console.log(status);
+					html = "<a href=\"http://twitch.tv/"+name+"\" target=\"blank\"><div class=\"row container "+status+" stream2\"><div class=\"col-3 logo_div\"><img class=\"logo\" src=\""+
+						channel.logo+"\"></div><div class=\"col-9 stream_div\"><h4>"+
+						channel.display_name+"<br><span class=\"stream_status\">"+
+						stream+"</span></h4></div></div></a>";
+					$("#streamers").append(html);
+			});
+			});
 		});
-		$.getJSON(channelApi,function(data2){
-			channel = data2;
-			console.log(stream);
-			html = "<a href=\"http://twitch.tv/"+name+"\" target=\"blank\"><div class=\"row container "+status+" stream2\"><div class=\"col-3 logo_div\"><img class=\"logo\" src=\""+
-				channel.logo+"\"></div><div class=\"col-9 stream_div\"><h4>"+
-				channel.display_name+"<br><span class=\"stream_status\">"+
-				stream+"</span></h4></div></div></a>";
-			$("#streamers").append(html);
-		});
-	});
+	}
+	create();
 	$("#left_status").click(function(){
 		$(".online").show();
 		$(".offline").show();
