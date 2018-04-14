408a76869f617040be5742d979b8d1dcc2d508ce
gdezan <33992304+gdezan@users.noreply.github.com>
2018-04-13 21:08:05 -0300

    added getjson calls and other major changes

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
 	var displayName = ["freecodecamp", "streamerhouse", "shroud"]
-	for (var i=0; i<displayName.length; i++){
-		var api = "https://wind-bow.glitch.me/twitch-api/channels/"+displayName[i];
-		$.ajax({
-			type: 'GET',
-			url: api,
-			datatype: "json",
-			success: function(data){
-				$("#streamers").append("<li class=\"stream\"><img class=\"logo\" src=\""+data.logo+"\"><span class=\"stream_name\">"+data.display_name+"</span></li>");
-		}
+	displayName.forEach(function(name){
+		var channelApi = "https://wind-bow.glitch.me/twitch-api/channels/"+name;
+		var streamApi = "https://wind-bow.glitch.me/twitch-api/streams/"+name;
+		var stream = '';
+		var html = '';
+		$.getJSON(streamApi, function(data){
+			data3 = data;
+			if (data.stream ===  null) {
+				stream = "Offline";
+			} else {
+				stream = "Playing "+data3.stream.game+" - "+data3.stream.viewers+" viewers";
+			}
+		});
+		$.getJSON(channelApi,function(data2){
+			channel = data2;
+			console.log(stream);
+			html = "<a href=\"http://twitch.tv/"+name+"\" target=\"blank\"><div class=\"row container stream2\"><div class=\"col-3 logo_div\"><img class=\"logo\" src=\""+
+				channel.logo+"\"></div><div class=\"col-9 stream_div\"><h4>"+
+				channel.display_name+"<br><span class=\"stream_status\">"+
+				stream+"</span></h4></div></div></a>";
+			$("#streamers").append(html);
+		});
 	});
-	}
 });
