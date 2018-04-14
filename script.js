39d8fbe0ad5f055ee581c3e407eebf9599197fa3
gdezan <33992304+gdezan@users.noreply.github.com>
2018-04-14 03:34:16 -0300

    added the option to add another streamer

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 			$.getJSON(streamApi, function(data){
 				var status = '';
 				statusData = data;
+				console.log(statusData.stream);
 				if (statusData.stream ===  null) {
 					stream = "Offline";
 					status = "offline";
@@ -19,7 +20,6 @@ $(document).ready(function() {
 
 				$.getJSON(channelApi,function(data2){
 					channel = data2;
-					console.log(status);
 					html = "<a href=\"http://twitch.tv/"+name+"\" target=\"blank\"><div class=\"row container "+status+" stream2\"><div class=\"col-3 logo_div\"><img class=\"logo\" src=\""+
 						channel.logo+"\"></div><div class=\"col-9 stream_div\"><h4>"+
 						channel.display_name+"<br><span class=\"stream_status\">"+
@@ -42,4 +42,14 @@ $(document).ready(function() {
 		$(".offline").show();
 		$(".online").hide();
 	});
+
+	$("#form").keypress(function(e) {
+		if(e.which==13){
+			var newStream = $("#form").val();
+			displayName.push(newStream);
+			$("#streamers").html('');
+			create();
+		}
+	});
+
 });
