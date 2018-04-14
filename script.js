04eddcdd0fa03d79a280e425d0ba9c87546068f4
gdezan <33992304+gdezan@users.noreply.github.com>
2018-04-13 23:16:59 -0300

    added status tab functionality

--- script.js
+++ script.js
@@ -9,18 +9,32 @@ $(document).ready(function() {
 			data3 = data;
 			if (data.stream ===  null) {
 				stream = "Offline";
+				status = "offline"
 			} else {
 				stream = "Playing "+data3.stream.game+" - "+data3.stream.viewers+" viewers";
+				status = "online"
 			}
 		});
 		$.getJSON(channelApi,function(data2){
 			channel = data2;
 			console.log(stream);
-			html = "<a href=\"http://twitch.tv/"+name+"\" target=\"blank\"><div class=\"row container stream2\"><div class=\"col-3 logo_div\"><img class=\"logo\" src=\""+
+			html = "<a href=\"http://twitch.tv/"+name+"\" target=\"blank\"><div class=\"row container "+status+" stream2\"><div class=\"col-3 logo_div\"><img class=\"logo\" src=\""+
 				channel.logo+"\"></div><div class=\"col-9 stream_div\"><h4>"+
 				channel.display_name+"<br><span class=\"stream_status\">"+
 				stream+"</span></h4></div></div></a>";
 			$("#streamers").append(html);
 		});
+	});
+	$("#left_status").click(function(){
+		$(".online").show();
+		$(".offline").show();
+	});
+	$("#middle_status").click(function(){
+		$(".online").show();
+		$(".offline").hide();
+	});
+	$("#right_status").click(function(){
+		$(".offline").show();
+		$(".online").hide();
 	});
 });
